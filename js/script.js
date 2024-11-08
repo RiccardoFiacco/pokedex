ec18f58c9ddad0ceadd02a900379539701a5429d
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-11-09 00:03:00 +0100

    add code to take the pokemon and add all in the card

--- js/script.js
+++ js/script.js
@@ -1,29 +1,60 @@
 
-function axiosCall(callback, n){
-    const staticUrl = "https://pokeapi.co/api/v2/pokemon/";
+function axiosCall(api, callback, n){
     const limit = "?limit="+n;
     axios
-    .get(staticUrl + limit)
+    .get(api + limit)
+    .then((res) => {
+        pkmn = res.data.results //per prendere un array di oggetti contenenti nome del pokemon e url della sua API
+        // callback(pkmn)
+        pkmn.forEach(el => {
+            console.log(el.name)
+            console.log(el.url)
+            axiosPokeCall(el.url, createCard)         
+        });
+        
+    })
+    .catch((err) => console.log(err));
+}
+
+
+function axiosPokeCall(api, callback){
+    axios
+    .get(api)
     .then((res) => {
         pkmn = res.data
-        console.log(pkmn.results)//per prendere un array di oggetti contenenti i pokemon
+        console.log(pkmn)
+        console.log(pkmn.id)
+        console.log(pkmn.height, pkmn.weight) 
+        console.log(pkmn.sprites.front_default)//img
+        console.log(pkmn.types.forEach((el)=> {console.log(el.type.name)}))
         callback(pkmn)
     })
     .catch((err) => console.log(err));
 }
 
-function createCards(array){
-    array.forEach(el => {
-        console.log(el)
-        cards.innerHTML +=
-        `<div class="card">
-            <img src="" class="card-img-top aspect-ratio-1 p-15px" alt="...">                    
-            <p class="pl-10px fw-bolder"></p>
-        </div>`
-    });
+function putCards(app){
+    console.log(app)
+    cards.innerHTML += app;
 }
+
+
+function createCard(obj){
+        cards.innerHTML += `
+        <div class="col-12 col-sm-6 col-md-4 col-xl-2">
+            <div class="card" id= ${obj.id}>
+                <img src="${obj.sprites.front_default}" class="card-img-top aspect-ratio-1 p-15px" alt="...">                    
+                <p class="pl-10px fw-bolder">${obj.name}</p>
+            </div>
+        </div>`
+        console.log(string)
+} 
+
+
+
 
 //chiamo una funzione passandogli una callaback per creare le cards con la risposta della chiamata axios 
 let cardNum = 3;
 let pkmn;
-axiosCall(createCards, cardNum);
+let string ="";
+const staticUrl = "https://pokeapi.co/api/v2/pokemon/";
+axiosCall(staticUrl, putCards, cardNum);
