97b485405e334b11a23b7aaa8f472b8c6f70743e
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-11-12 20:24:38 +0100

    create the script for the second page

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,5 +1,32 @@
-import { sendId }  from "./script.js";
+function axiosPokeCall(api, callback){
+    axios
+    .get(api)
+    .then((res) => {
+        let pkmn = res.data
+        callback(pkmn)
+    })
+    .catch((err) => console.log(err));
+}
+
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get('id');
+
 window.addEventListener("load", (event) => {
     event.preventDefault()
-    console.log( sendId)
+
+    axiosPokeCall('https://pokeapi.co/api/v2/pokemon/' + id, (pokemon) => {
+        pkmnName.innerText += pokemon.name;
+        pokeId.innerText += " "+pokemon.id;
+        height.innerText += " "+pokemon.height;
+        weigth.innerText += " "+pokemon.weight;
+        pkmnImg.src = pokemon.sprites.front_default;
+
+        let arr = pokemon.types
+        arr.forEach(el => {
+            type.innerText += " " + el.type.name
+        });
+
+        cry.src = pokemon.cries.latest;
+    })
+    
 })
