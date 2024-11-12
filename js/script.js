091e82e161fa15a5922773c6f33b911df33fbbe7
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-11-12 19:48:54 +0100

    change the main script

--- js/script.js
+++ js/script.js
@@ -4,64 +4,38 @@ function axiosCall(n){
     axios
     .get(staticUrl+ limit)
     .then((res) => {
-        pkmn = res.data.results //per prendere un array di oggetti contenenti nome del pokemon e url della sua API
-        pkmn.forEach(el => { //per ogni pokemon effet
-            axiosPokeCall(el.url, createCard)         
+        let pkmn = res.data.results //per prendere un array di oggetti contenenti nome del pokemon e url della sua API
+        pkmn.forEach(el => { //per ogni pokemon 
+            axiosPokeCall(el.url, createCard) //fai una chiamata all'api contenuta dentro l'obj pokemon e gli passi una callback per creare una card
         });
-        
     })
     .catch((err) => console.log(err));
 }
 
 
-function axiosPokeCall(api, callback){
+function axiosPokeCall(api, callback){ //funzione per chiamare le singole api dei pokemon
     axios
     .get(api)
-    .then((res) => {
-        let pkmn = res.data
-        // console.log(pkmn)
-        // console.log(pkmn.id)
-        // console.log(pkmn.height, pkmn.weight) 
-        // console.log(pkmn.sprites.front_default)//img
-        // console.log(pkmn.types.forEach((el)=> {console.log(el.type.name)}))
-        callback(pkmn)
+    .then((res) => { 
+        let pkmn = res.data //prendi i dati del pokemon e li passi alla variabile pkmn
+        callback(pkmn) //mandi alla callback createCard
     })
     .catch((err) => console.log(err));
 }
 
-// function putCards(app){
-//     console.log(app)
-//     cards.innerHTML += app;
-// }#
-
-function createCard(obj){
-        cards.innerHTML += `
+function createCard(obj){ //crea la card e la mette nel html
+    cards.innerHTML += `
         <div class="col-12 col-sm-6 col-md-4 col-xl-2">
-            <a href="pokemon.html">   
-                <div class="card hvr-grow" id= ${obj.id}>
+            <a href="pokemon.html?id=${obj.id}">   
+                <div class="card hvr-grow" id="${obj.id}">
                     <img src="${obj.sprites.front_default}" class="card-img-top aspect-ratio-1 p-15px" alt="...">                    
                     <p class="pl-10px fw-bolder">${obj.name}</p>
                 </div>
             </a>
         </div>`
-        addListener()
 } 
-
-function addListener(){
-    let els = document.querySelectorAll(".card"); //seleziono tutte le card con la la casse card
-    els.forEach((el)=>{ //per ogni card
-        el.addEventListener("click", function(){ //aggiungo una funzione che al click
-            sendId = el.getAttribute("id") //mi prende il valore di id
-            console.log(sendId)
-        })
-    })
-}
 
 //chiamo una funzione passandogli una callaback per creare le cards con la risposta della chiamata axios 
 let cardNum = 151;
-let pkmn;
-let sendId;
 const staticUrl = "https://pokeapi.co/api/v2/pokemon/";
 axiosCall(cardNum);
-
-export { sendId }
